a86de482fda03a9cb2c6a94a556841d022bafd73
Nukecraft5419 <github.c8b49@slmail.me>
2022-10-13 06:14:47 +0200

    Updated

---
+++ js/modrinth.js
@@ -0,0 +1,12 @@
+var url = "https://api.modrinth.com/v2/project/sodium",
+  id = "...",
+  slug = "...",
+  project_type = "mod",
+  team = "4reLOAKe",
+  title = "Sodium",
+  description = "Modern rendering engine and client-side optimization mod for Minecraft",
+  body = "...",
+  body_url = "https://cdn.modrinth.com/data/AANobbMI/description.md",
+  published = "2021-01-03T00:53:34.185936Z",
+  updated = "2022-01-14T02:35:40.373686Z";
+xhr.open("POST", url);
